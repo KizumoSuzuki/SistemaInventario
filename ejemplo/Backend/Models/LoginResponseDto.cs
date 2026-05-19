582df90b893a2d88f6d6b4047698feda7628ee7d
Carlos Reynoso <141469420+KizumoSuzuki@users.noreply.github.com>
2026-05-19 00:44:02 -0400

    Delete ejemplo/Backend directory

--- ejemplo/Backend/Models/LoginResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CRUD.Models
-{
-    public class LoginResponseDto
-    {
-        public int Id { get; set; }
-        public string Mensaje { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
-    }
-}
